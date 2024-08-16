4d3eab516de9c322a1ef3cfc8b61ac657618a7a4
christophef <christophe.feuillard78320@gmail.com>
2024-08-16 23:15:24 +0200

    feat: page home

---
+++ client/app/lib/events-data.js
@@ -0,0 +1,52 @@
+const events = [
+  {
+    id: 1,
+    title: 'Rock En Seine 2024',
+    location: 'Rock En Seine',
+    date: 'mer 21 - dim 25 août',
+    vibe: ['pop', 'rock', 'electro'],
+    price: '61,00',
+  },
+  {
+    id: 2,
+    title: 'Rock En Seine 2024',
+    location: 'Rock En Seine',
+    date: 'mer 21 - dim 25 août',
+    vibe: ['pop', 'rock', 'electro'],
+    price: '61,00',
+  },
+  {
+    id: 3,
+    title: 'Rock En Seine 2024',
+    location: 'Rock En Seine',
+    date: 'mer 21 - dim 25 août',
+    vibe: ['pop', 'rock', 'electro'],
+    price: '61,00',
+  },
+  {
+    id: 4,
+    title: 'Rock En Seine 2024',
+    location: 'Rock En Seine',
+    date: 'mer 21 - dim 25 août',
+    vibe: ['pop', 'rock', 'electro'],
+    price: '61,00',
+  },
+  {
+    id: 5,
+    title: 'Rock En Seine 2024',
+    location: 'Rock En Seine',
+    date: 'mer 21 - dim 25 août',
+    vibe: ['pop', 'rock', 'electro'],
+    price: '61,00',
+  },
+  {
+    id: 6,
+    title: 'Rock En Seine 2024',
+    location: 'Rock En Seine',
+    date: 'mer 21 - dim 25 août',
+    vibe: ['pop', 'rock', 'electro'],
+    price: '61,00',
+  },
+];
+
+export default events;
